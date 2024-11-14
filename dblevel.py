b85beb3d7c6a5b6e04059dbb5988b516be64aec7
usiqwerty <benlogin1@protonmail.com>
2024-11-14 13:04:22 +0500

    Remove redundant code

--- dblevel.py
+++ dblevel.py
@@ -2,7 +2,7 @@ import os
 from typing import Iterable
 
 import sqlalchemy
-from sqlalchemy import create_engine, Engine, Connection, select, insert, table, Table
+from sqlalchemy import create_engine, Engine, select
 from sqlalchemy.orm import sessionmaker
 
 from course import Course, CourseRequestSchema, Base
@@ -10,25 +10,20 @@ from course import Course, CourseRequestSchema, Base
 HOSTNAME = 'localhost'
 PASSWORD = os.environ["POSTGRES_PASS"]
 USERNAME = 'terciadb'
+DB_NAME = 'terciadb'
 
 
 class Database:
     engine: Engine
-    # cursor: Connection
     session: sqlalchemy.orm.Session
-    table: Table
 
     def __init__(self):
         pass
 
     def connect(self):
-        self.engine = create_engine(f'postgresql://{USERNAME}:{PASSWORD}@{HOSTNAME}/terciadb')  # +psycopg2
+        self.engine = create_engine(f'postgresql://{USERNAME}:{PASSWORD}@{HOSTNAME}/{DB_NAME}')
         Base.metadata.create_all(self.engine)
-        Session = sessionmaker(bind=self.engine)
-        # self.cursor = self.engine.connect()
-        self.session = Session()
-
-        self.table = Base.metadata.tables["courses"]
+        self.session = sessionmaker(bind=self.engine)()
 
     def get_courses(self) -> Iterable[CourseRequestSchema]:
         query = select(Course)
@@ -36,7 +31,5 @@ class Database:
             yield CourseRequestSchema(id=course.id, name=course.name, cover_url=course.cover_url)
 
     def add_course(self, course: CourseRequestSchema):
-        # query = insert().values(name=course.name, cover_url=course.cover_url)
-
         self.session.add(Course(name=course.name, cover_url=course.cover_url))
         self.session.commit()
